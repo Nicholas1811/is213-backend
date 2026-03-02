6a91ee9a39b79070068c56cead29618239b74b6c
Nicholas <nicholassdl660@gmail.com>
2026-03-02 21:28:40 +0800

    feat: notif system is up

--- services/Notificationmanagement-service/dto/notification.py
+++ services/Notificationmanagement-service/dto/notification.py
@@ -1,13 +1,18 @@
 class Notification():
-    def __init__(self, title, message, notificationType, userId):
+    def __init__(self, title, message, notificationType, userId, event_id):
         self.title = title
         self.message = message
         self.notification_type = notificationType
         self.userId = userId
+        self.event_id = event_id
     def notification_body(self):
-        return{
-            "title" : self.title,
-            "message" : self.message,
-            "notficiation_type" : self.notification_type,
-            "userId" : self.userId
-        }
+        return {
+                    "title" : self.title,
+                    "message" : self.message,
+                    "notification_type" : self.notification_type,
+                    "userId" : self.userId,
+                    "event_uuid" : self.event_id
+                }
+
+
+
