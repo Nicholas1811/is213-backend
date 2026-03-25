bf859b45802905e9bb8c2acedd7259d569283063
Dwayne Otero <daa.otero.2024@computing.smu.edu.sg>
2026-03-25 21:54:08 +0800

    feat: implemented refund service for stripe and points

---
+++ services/Refund-service/class_model/input_model.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+
+#  this is the expected json input either or both can be provided
+# {
+#   "order_id": 2,
+#   "user_id": "550e8400-e29b-41d4-a716-446655440000",
+#   "points_amount": 100,
+#   "point_id": "f8392488-5ecf-44d9-9483-1642dfea5dc4",
+#   "payment_intent_id": "pi_3S64492eZvKYlo2C13313131"
+# }
+
+class RefundRequest(BaseModel):
+    order_id: int
+    user_id: str
+    points_amount: Optional[int] = None
+    point_reference_id: Optional[str] = None
+    payment_intent_id: Optional[str] = None
