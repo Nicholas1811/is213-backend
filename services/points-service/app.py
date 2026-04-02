3ea2523260436ec376ebf53111b3fd9a217ed126
Nicholas <nicholassdl660@gmail.com>
2026-04-03 00:08:35 +0800

    fix to point db

--- services/points-service/app.py
+++ services/points-service/app.py
@@ -10,7 +10,7 @@ import services
 load_dotenv("../.env")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POINT_DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
